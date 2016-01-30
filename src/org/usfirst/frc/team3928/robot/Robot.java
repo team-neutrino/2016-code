1b0c6c2f1b59df7ae7c390e1f93bb32a869227c4
Isaac <ihstahr@gmail.com>
2016-01-30 13:41:44 -0600

    Something's wrong...

---
+++ src/org/usfirst/frc/team3928/robot/Robot.java
@@ -0,0 +1,87 @@
+
+package org.usfirst.frc.team3928.robot;
+
+
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.SampleRobot;
+import edu.wpi.first.wpilibj.Talon;
+//import edu.wpi.first.wpilibj.RobotDrive;
+//import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.Timer;
+//import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
+//import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+
+/**
+ * This is a demo program showing the use of the RobotDrive class.
+ * The SampleRobot class is the base of a robot application that will automatically call your
+ * Autonomous and OperatorControl methods at the right time as controlled by the switches on
+ * the driver station or the field controls.
+ *
+ * The VM is configured to automatically run this class, and to call the
+ * functions corresponding to each mode, as described in the SampleRobot
+ * documentation. If you change the name of this class or the package after
+ * creating this project, you must also update the manifest file in the resource
+ * directory.
+ *
+ * WARNING: While it may look like a good choice to use for your code if you're inexperienced,
+ * don't. Unless you know what you are doing, complex code will be much more difficult under
+ * this system. Use IterativeRobot or Command-Based instead if you're new.
+ */
+public class Robot extends SampleRobot {
+    
+	Joystick joyLeft;
+	Joystick joyRight;
+	Joystick game;
+	Drive driver;
+	
+    public Robot() {
+    	joyLeft = new Joystick(Constants.JOY_LEFT.getInt());
+		joyRight = new Joystick(Constants.JOY_RIGHT.getInt());
+		game = new Joystick(Constants.GAMEPAD.getInt());
+		driver = new Drive();
+    }
+    
+    public void robotInit() {
+        
+    }
+    public void disabled(){
+    	
+    	
+    }
+	/**
+	 * This autonomous (along with the chooser code above) shows how to select between different autonomous modes
+	 * using the dashboard. The sendable chooser code works with the Java SmartDashboard. If you prefer the LabVIEW
+	 * Dashboard, remove all of the chooser code and uncomment the getString line to get the auto name from the text box
+	 * below the Gyro
+	 *
+	 * You can add additional auto modes by adding additional comparisons to the if-else structure below with additional strings.
+	 * If using the SendableChooser make sure to add them to the chooser code above as well.
+	 */
+    public void autonomous() {
+    	
+    	
+    }
+
+    /**
+     * Runs the motors with arcade steering.
+     */
+    public void operatorControl() {
+        
+    	while (isOperatorControl() && isEnabled()) {
+    		double leftSpeed = joyLeft.getY();
+			double rightSpeed = joyRight.getY();
+			driver.setLeftSpeed(Math.abs(leftSpeed));
+			driver.setRightSpeed(Math.abs(rightSpeed));
+            
+            Timer.delay(0.005);		// wait for a motor update time
+        }
+    }
+
+    /**
+     * Runs during test mode
+     */
+    public void test()
+    {
+    	
+    }
+}
